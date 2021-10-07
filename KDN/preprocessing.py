c0c1f22fd8e2cef42d8db969af7a13544b779c79
nid2S <s20034@gsm.hs.kr>
2021-10-07 21:16:56 +0900

    test

--- KDN/preprocessing.py
+++ KDN/preprocessing.py
@@ -42,7 +42,7 @@ def make_label_dict():
 
 
 def train():
-    # 88971
+    # 88970
     # get first train data
     t1 = pd.read_csv("./dataset/EmotionalTalkCorpusTraining1.txt", sep="\t", encoding="949")
     drop_columns = ["번호", "연령", "value", "성별", "상황키워드", "신체질환", '시스템응답1', '사람문장2', '시스템응답2', '사람문장3', '시스템응답3']
@@ -71,13 +71,13 @@ def train():
     train_data.to_csv("./dataset/Train.txt", sep="\t", index=False, header=["emotion_b", "emotion_d", "sent"])
 
 def validation():
-    # 13984
-    # get first train data
+    # 13983
+    # get first validation data
     v1 = pd.read_csv("./dataset/EmotionalTalkCorpusValidation1.txt", sep="\t", encoding="949")
     drop_columns = ["번호", "연령", "value", "성별", "상황키워드", "신체질환", '시스템응답1', '사람문장2', '시스템응답2', '사람문장3', '시스템응답3']
     v1.drop(drop_columns, axis=1, inplace=True)
     v1.dropna(inplace=True)
-    # get second train data
+    # get second validation data
     v2 = pd.read_csv("./dataset/EmotionalTalkCorpusValidation2.txt", sep="\t", encoding="949")
     drop_columns = ["번호", "연령", "성별", "상황키워드", "신체질환", '시스템응답1', '사람문장2', '시스템응답2', '사람문장3', '시스템응답3', '사람문장4', '시스템응답4']
     v2.drop(drop_columns, axis=1, inplace=True)
@@ -101,3 +101,35 @@ def validation():
 
     val_data.to_csv("./dataset/Validation.txt", sep="\t", index=False, header=["emotion_b", "emotion_d", "sent"])
 
+def test():
+    # 9376
+    # get test data
+    test_data = pd.read_csv("./dataset/EmotionalTalkCorpusTest.txt", sep="\t", encoding="949")
+    drop_columns = ["연령", "성별", "상황키워드", '기계문장1', '사람문장2', '기계문장2', '사람문장3', '기계문장3']
+    unnamed_columns = ['Unnamed: 11', 'Unnamed: 12', 'Unnamed: 13', 'Unnamed: 14', 'Unnamed: 15', 'Unnamed: 16', 'Unnamed: 17', 'Unnamed: 18']
+    # columns = ['감정_대분류', '감정_소분류', '사람문장1']
+    test_data.drop(drop_columns + unnamed_columns, axis=1, inplace=True)
+    test_data.dropna(inplace=True)
+    test_data.drop_duplicates(["사람문장1"], inplace=True)
+    test_data = test_data.reset_index().drop(["index"], axis=1)
+    # get label_dict
+    b_dict = dict([(row["Emotion"], row["index"]) for _, row in pd.read_csv("./dataset/EmotionB.txt", sep="\t").iterrows()])
+    d_dict = dict([(row["Emotion"], row["index"]) for _, row in pd.read_csv("./dataset/EmotionD.txt", sep="\t").iterrows()])
+    # preprocessing
+    for idx, row in test_data.iterrows():
+        # 감정_소분류 | 형에 맞게 변환 후 정수 인코딩
+        # '당황_고립된(당황한)', '당황_혼란스러운(당황한)', '당황_괴로워하는' 제거
+        row["감정_소분류"] = row["감정_소분류"].replace("(당황한)", "")
+        row["감정_소분류"] = row["감정_소분류"].replace("괴로워하는", "괴로워 하는")
+        try:
+            row["감정_소분류"] = d_dict[row["감정_대분류"] + "_" + row["감정_소분류"]]
+        except KeyError:
+            # '당황_분노', '분노_구역질나는', '분노_스트레스 받는', '분노_희생된', '불안_스트레스받는',
+            # '상처_외로운', '상처_충격받은', '슬픔_상처', '슬픔_성가신', '슬픔_열등감', '슬픔_외로운' 의 경우, 해당 열 제거
+            test_data.drop(idx, inplace=True)
+        # 감정_대분류 | 정수 인코딩
+        row["감정_대분류"] = b_dict[row["감정_대분류"]]
+
+    test_data.to_csv("./dataset/Test.txt", sep="\t", index=False, header=["emotion_b", "emotion_d", "sent"])
+
+test()
